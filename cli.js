deb137c8c8915a07ef13d96171e9bc6fd52e7f70
Shai Yallin <shai.yallin@gmail.com>
2020-01-21 11:02:15 +0200

    added validator data

--- cli.js
+++ cli.js
@@ -2,13 +2,20 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require("web3");
 const request = require("request-promise");
 const {getStatus, getEndpoint} = require("@orbs-network/orbs-nebula/lib/metrics");
-const { table } = require("table");
+const {table} = require("table");
 
 const mnemonic = "a47468deab664d3ba2aec1db6787ce84";
 const provider = new HDWalletProvider(mnemonic, `http://eth.orbs.com`, 0, 25);
 const web3 = new Web3(provider);
+
 const mainnetTopologyContractAddress = "0x804c8336846d8206c95CEe24752D514210B5a240";
 const topologyContractAbi = require("./build/contracts/OrbsValidators");
+
+const mainnetValidatorRegistryContractAddress = "0x56A6895FD37f358c17cbb3F14A864ea5Fe871F0a";
+const validatorRegistryContractAbi = require("./build/contracts/OrbsValidatorRegistry");
+
+const topologyContract = new web3.eth.Contract(topologyContractAbi, mainnetTopologyContractAddress);
+const validatorRegistry = new web3.eth.Contract(validatorRegistryContractAbi, mainnetValidatorRegistryContractAddress);
 
 function hexStringToIPAddress(str) {
     if (!str) {
@@ -47,7 +54,7 @@ async function getStatusFor(address, ip, vchain) {
         address,
         ip,
         vchain,
-        status: status.data
+        status: status.data,
     }
 }
 
@@ -65,36 +72,57 @@ function reportStatus({status}) {
     }
 }
 
-function toTable(results, vchains, ips, addresses) {
+async function getValidatorData(address) {
+    const ethAddress = await validatorRegistry.methods.lookupByOrbsAddr(address).call();
+    const validatorData = await validatorRegistry.methods.getValidatorData(ethAddress).call();
+    return validatorData;
+
+}
+
+function toTable(results, vchains, ips, addresses, validatorsData) {
     const data = [];
-    data.push(["Node Address", "IP", ...vchains]);
+    data.push(["Validator", "IP", ...vchains]);
     ips.forEach((ip, i) => {
         const vchainStatuses = results.filter(r => r.ip === ip).map(reportStatus);
-        data.push([addresses[i], ip, ...vchainStatuses]);
+        const validatorData = validatorsData[addresses[i]];
+        const validatorDescription = `${validatorData.name}\nNode Address: ${validatorData.orbsAddress}\n${validatorData.website}`;
+        data.push([validatorDescription, ip, ...vchainStatuses]);
     });
     return table(data);
 }
 
+async function queryActiveVChains() {
+    const centralizedConfig = await getConfig("http://orbs-bootstrap-prod.s3.us-east-1.amazonaws.com/boyar/config.json");
+    return centralizedConfig.chains.filter(vchain => !vchain.Disabled).map(vchain => vchain.Id);
+}
+
+async function queryTopology() {
+    const topology = await topologyContract.methods.getNetworkTopology().call();
+    const ips = topology.ipAddresses.map(hexStringToIPAddress);
+    const addresses = topology.nodeAddresses;
+    return {ips, addresses};
+}
+
+function queryValidatorStatuses(vchains, ips, addresses) {
+    const statuses = [];
+    vchains.forEach(vchain => {
+        ips.forEach((ip, i) => {
+            statuses.push(getStatusFor(addresses[i], ip, vchain));
+        });
+    });
+    return Promise.all(statuses);
+}
+
+async function queryValidatorsData(addresses) {
+    const validatorDataArray = await Promise.all(addresses.map(getValidatorData));
+    return validatorDataArray.reduce((a, b) => (a[b.orbsAddress] = b, a), {});
+}
+
 (async function () {
     try {
-        const centralizedConfig = await getConfig("http://orbs-bootstrap-prod.s3.us-east-1.amazonaws.com/boyar/config.json");
-        const vchains = centralizedConfig.chains.filter(vchain => !vchain.Disabled).map(vchain => vchain.Id);
-
-        const topologyContract = new web3.eth.Contract(topologyContractAbi, mainnetTopologyContractAddress);
-        const topology = await topologyContract.methods.getNetworkTopology().call();
-        const ips = topology.ipAddresses.map(hexStringToIPAddress);
-        const addresses = topology.nodeAddresses;
-
-        const statuses = [];
-        vchains.forEach(vchain => {
-            ips.forEach((ip, i) => {
-                statuses.push(getStatusFor(addresses[i], ip, vchain));
-            });
-        });
-
-        const results = await Promise.all(statuses);
-        console.log(toTable(results, vchains, ips, addresses));
-
+        const [vchains, {ips, addresses}] = await Promise.all([queryActiveVChains(), queryTopology()]);
+        const [statuses, validatorData] = await Promise.all([queryValidatorStatuses(vchains, ips, addresses), queryValidatorsData(addresses)]);
+        console.log(toTable(statuses, vchains, ips, addresses, validatorData));
 
     } finally {
         provider.engine.stop();
